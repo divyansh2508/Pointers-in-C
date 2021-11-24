54b17aaa66fa8349d4317fd9dffc9696da5349db
Mridul Vasudeva <88312870+King-MV@users.noreply.github.com>
2021-11-24 19:21:44 +0530

    Add files via upload

---
+++ ArrayFunction.c
@@ -0,0 +1,32 @@
+// A C program to pass an array into a function using pointers.
+
+#include <stdio.h>
+
+void main () {
+    int n;
+    printf("Enter length of array: ");
+    scanf("%d",&n);
+
+    int arr[n];
+
+    // Loop used to take input of elements of array.
+    for (int i = 0; i<n; i++) {
+        printf("Enter %d element: ",i+1);
+        scanf("%d",arr+i);
+    }
+
+    ModifyArray(arr , n);
+
+    // Loop used to print the modified array.
+    for (int k = 0; k<n; k++) {
+        printf("%d ",*(arr+k));
+    }
+
+}
+
+void ModifyArray (int *arr , int n) {
+    // Loop used to modify the array.
+    for (int j=0; j<n; j++) {
+        *(arr+j) += 3;  
+    }
+}
