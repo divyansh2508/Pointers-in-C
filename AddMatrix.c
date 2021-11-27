e10bcb7c2d135742b2beb321d4cb9a3028725c22
Mridul Vasudeva <88312870+King-MV@users.noreply.github.com>
2021-11-27 21:37:30 +0530

    Add files via upload

---
+++ AddMatrix.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main () {
+    int m , n; 
+    printf("Enter size of Matrix [] []: ");
+    scanf("%d %d",&m,&n);
+
+    int arr1[m][n];
+    int arr2[m][n];
+
+    printf("\n\nEnter elements for 1st Matrix.\n");
+
+    // Loops used to input elements of 2D array "arr1".
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            printf("Enter [%d][%d] element: ",i,j);
+            scanf("%d",&arr1[i][j]);
+        }
+    }
+
+    // Loops used to print elements of array "arr1".
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            printf("%d ",*(*(arr1+i)+j));
+        }
+            printf("\n");
+    }
+
+    printf("\n\nEnter elements for 2nd Matrix.\n");
+
+    // Loops used to input elements of 2D array "arr2".
+    for (int k=0; k<m; k++) {
+        for (int l=0; l<n; l++) {
+            printf("Enter [%d][%d] element: ",k,l);
+            scanf("%d",&arr2[k][l]);
+        }
+    }
+
+    // Loops used to print elements of array "arr2".
+    for (int k=0; k<m; k++) {
+        for (int l=0; l<n; l++) {
+            printf("%d ",*(*(arr2+k)+l));
+        }
+        printf("\n");
+    }
+
+    printf("\n\nAdded Matrix is:\n");
+
+    // Loops used to add two matrix.
+    for (int x=0; x<m; x++) {
+        for (int y=0; y<n; y++) {
+            printf("%d ",*(*(arr1+x)+y) + *(*(arr2+x)+y));
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    return 0;
+}
