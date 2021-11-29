6ef8f3ecd9bd8461656d81f81e171db908e57ca4
Mridul Vasudeva <88312870+King-MV@users.noreply.github.com>
2021-11-29 21:41:04 +0530

    Update mul_numbers_pointer.c

--- mul_numbers_pointer.c
+++ mul_numbers_pointer.c
@@ -1,15 +1,48 @@
-/*WAP to multiply two number using pointer*/
+// A C program to multiply , add & subtract two numbers as per users choice.
+// Menu driven approach is used.
+
 #include <stdio.h>
 
 int main()
 {
-    int a,b,*p,*q,mul;
-    printf("enter the two number to multiply"); 
+    int a,b,*p,*q,mul,add,sub;
+    printf("Enter the two numbers: "); 
     scanf("%d%d",&a,&b);
     p = &a;
     q = &b;
-    mul = *p * *q;
-    printf("multiplication of two number = %d",mul);
+    
+    // Menu 
+    printf("\n\t\tEnter:\n");
+    printf("\t1 to Multiply two numbers\n");
+    printf("\t2 to Subtract two numbers\n");
+    printf("\t3 to Add two numbers\n");
+    
+    // Taking input as per users choice.
+    int ch;
+    printf("\nEnter your choice: ");
+    scanf("%d",&ch);
+    
+    // Using switch case statement to provide a menu driven outlook.
+    switch (ch) {
+        case 1: {
+            mul = (*p) * (*q);
+            printf("\nMultiplication of %d and %d is %d",a,b,mul);
+            // Break statement is used to stop the flow of program.
+            break;
+        }
+        case 2: {
+            sub = *p - *q;
+            printf("\nSubtraction of %d and %d is %d",a,b,sub);
+            // Break statement is used to stop the flow of program.
+            break;
+        }
+        case 3: {
+            add = *p + *q;
+            printf("\nAddition of %d and %d is %d",a,b,add);
+            // Break statement is used to stop the flow of program.
+            break;
+        }
+    }
 
     return 0;
 }
